7e7957d5593eac3db9716ccdb31e68aad185b3ff
JhonXY <1076849402@qq.com>
2016-10-10 12:25:56 +0800

    测试

---
+++ vs/vs/源.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "counting firward:\n";
+	for (int i = 0; i < 10; ++i)
+	{
+		cout << i << "";
+	}
+	cout << "\n\nCounting backward:\n";
+	for (int i = 9; i >= 0; --i)
+	{
+		cout << i << "";
+	}
+	cout << "\n\nCounting by fives:\n";
+	for
+}
